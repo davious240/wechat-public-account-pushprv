5898f0d89189baf840c797d752acdf93039924a2
davious240 <78192876+davious240@users.noreply.github.com>
2023-10-10 20:45:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小朋友',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oip1R52-bf9IcdD1xnMmi-dDoDkA',
+      id: 'oip1R503mvonjiqeOgu2h901tNKg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'rLHqHDLW4H-5mucjZd2rWDdw1u9jd4pUG59sq8gBNpA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
